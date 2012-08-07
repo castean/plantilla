cd2ef19171dd5ad28395ea3aceb54441694b620d
castean <castean@hotmail.com>
2012-08-07 12:49:14 -0600

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plantilla::Application.config.secret_token = 'b8532e4e33d51745d61b1a4b06092e7efd9a4415d87f69cff7b2f2bd98b7081abf0ae5412338df44b7b83d104a91a20b0acaebecc5822886ce5ed327d99f70e2'
