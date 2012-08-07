033c34171d1c74d093b1a628d2a4c3f9920b74ff
castean <castean@hotmail.com>
2012-08-07 13:12:54 -0600

    java script

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,10 @@
+$(function(){
+      $("#faded").faded({
+        speed: 900,
+        crossfade: true,
+        bigtarget: true,
+        autoplay: 4800,
+        autorestart: 1200,
+		autopagination: false
+      });
+    });
